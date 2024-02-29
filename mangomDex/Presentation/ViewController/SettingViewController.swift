2c6bf1bea518c579473ff2c133ed4d764b44d0fe
sjsjmine129 <sjsjmine129@gmail.com>
2024-02-29 23:59:27 +0900

    Feat/ Add pad UI to setting tab

--- mangomDex/Presentation/ViewController/SettingViewController.swift
+++ mangomDex/Presentation/ViewController/SettingViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class SettingViewController: UIViewController {
     
+    private lazy var padView: UIView = {
+        let vw = UIView()
+        vw.translatesAutoresizingMaskIntoConstraints = false
+        vw.backgroundColor = UIColor(named: Color.mag_body.rawValue)
+        vw.layer.cornerRadius = 10
+        
+        return vw
+    }()
+    
+    private lazy var appIcon: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.image = UIImage(named: "icon.png")
+        img.layer.cornerRadius = 20
+        img.layer.masksToBounds = true
+        
+        return img
+    }()
+    
     // MARK - LifeCycle
     override func loadView() {
         super.loadView()
         setNavigationBar()
+        setUI()
     }
 
     override func viewDidLoad() {
@@ -40,7 +60,27 @@ private extension SettingViewController{
         
         self.navigationController?.navigationBar.frame.size.height = 50
         self.navigationController?.navigationBar.backgroundColor = .clear
+    }
+    
+    //set UI
+    func setUI(){
         
+        self.view.addSubview(padView)
+        padView.addSubview(appIcon)
+        
+        NSLayoutConstraint.activate([
+            //padView
+            padView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            padView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            padView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            padView.heightAnchor.constraint(equalToConstant: 280),
+            //appIcon
+            appIcon.topAnchor.constraint(equalTo: padView.topAnchor, constant: 30),
+            appIcon.centerXAnchor.constraint(equalTo: padView.centerXAnchor),
+            appIcon.heightAnchor.constraint(equalToConstant: 90),
+            appIcon.widthAnchor.constraint(equalToConstant: 90),
+            
+        ])
         
     }
 }
