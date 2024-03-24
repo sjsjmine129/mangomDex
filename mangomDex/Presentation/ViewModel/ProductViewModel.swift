bdfa9e748280b2861aa02659f773f9566f6a35cd
sjsjmine129 <sjsjmine129@gmail.com>
2024-03-24 18:32:47 +0900

    Fix

--- mangomDex/Presentation/ViewModel/ProductViewModel.swift
+++ mangomDex/Presentation/ViewModel/ProductViewModel.swift
@@ -10,14 +10,14 @@ import Foundation
 class ProductViewModel{
     
     private(set) var products: [Product] = [
-        Product(name: "망곰이의딸기샌드", price: 3600, findLink: "https://pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809895798564", productLink: "https://pocketcu.co.kr/product/detail/2023120035505?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰초코딸기샌드", price: 3600, findLink: "https://pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809895798526", productLink: "https://pocketcu.co.kr/product/detail/2024010036989?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰이초코마카롱", price: 3800, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955214", productLink: "https://www.pocketcu.co.kr/product/detail/2023110035442?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰이딸기마카롱", price: 3800, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955191", productLink: "https://www.pocketcu.co.kr/product/detail/2023110035438?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰이의행운버거", price: 3800, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955283", productLink: "https://www.pocketcu.co.kr/product/detail/2023120036323?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰이의꿀호떡버거", price: 3400, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955115", productLink: "https://www.pocketcu.co.kr/product/detail/2023110035285?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰치즈감자베이글", price: 4900, productLink: "https://www.pocketcu.co.kr/product/detail/2024010036642?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
-        Product(name: "망곰바질토마토베이글", price: 4900 ,productLink: "https://pocketcu.co.kr/product/detail/2023120035619?cateTyp=D"),
+        Product(productName: "망그러진 곰 우유슈크림 딸기샌드", name: "망곰이의딸기샌드", price: 3600, findLink: "https://pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809895798564", productLink: "https://pocketcu.co.kr/product/detail/2023120035505?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 초코생딸기 페스츄리", name: "망곰초코딸기샌드", price: 3600, findLink: "https://pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809895798526", productLink: "https://pocketcu.co.kr/product/detail/2024010036989?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 쫀득한 초코마카롱", name: "망곰이초코마카롱", price: 3800, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955214", productLink: "https://www.pocketcu.co.kr/product/detail/2023110035442?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 쫀득한 딸기마카롱", name: "망곰이딸기마카롱", price: 3800, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955191", productLink: "https://www.pocketcu.co.kr/product/detail/2023110035438?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 행운버거", name: "망곰이의행운버거", price: 3800, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955283", productLink: "https://www.pocketcu.co.kr/product/detail/2023120036323?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 꿀호떡버거", name: "망곰이의꿀호떡버거", price: 3400, findLink: "https://www.pocketcu.co.kr/search/stock?isRecommend=Y&item_cd=8809692955115", productLink: "https://www.pocketcu.co.kr/product/detail/2023110035285?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 치즈포테이토 베이글", name: "망곰치즈감자베이글", price: 4900, productLink: "https://www.pocketcu.co.kr/product/detail/2024010036642?store_cd=&cateTyp=&chldMealEvtYn=&isNetYn=Y"),
+        Product(productName: "망그러진 곰 바질토마토 베이글", name: "망곰바질토마토베이글", price: 4900 ,productLink: "https://pocketcu.co.kr/product/detail/2023120035619?cateTyp=D"),
 
     ]
 }
