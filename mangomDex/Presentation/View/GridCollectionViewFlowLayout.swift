efe0c3502f8e867f45b78de9b7ba764c4dfd3530
sjsjmine129 <sjsjmine129@gmail.com>
2024-03-30 23:35:05 +0900

    Fix/buffering

--- mangomDex/Presentation/View/GridCollectionViewFlowLayout.swift
+++ mangomDex/Presentation/View/GridCollectionViewFlowLayout.swift
@@ -16,52 +16,52 @@ class GridCollectionViewFlowLayout: UICollectionViewFlowLayout {
             self.minimumInteritemSpacing = self.cellSpacing
         }
     }
-    
-    private var pinchGestureRecognizer: UIPinchGestureRecognizer!
-    private var initialPinchScale: CGFloat = 1.0
-    private var columns: Int = 4
-    
-    override func prepare() {
-        super.prepare()
-        setupPinchGestureRecognizer()
-    }
-    
-    private func setupPinchGestureRecognizer() {
-        pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture(_:)))
-        pinchGestureRecognizer.delegate = self
-        collectionView?.addGestureRecognizer(pinchGestureRecognizer)
-    }
-    
-    @objc private func handlePinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
-        guard collectionView != nil else { return }
-        switch gestureRecognizer.state {
-        case .began:
-            initialPinchScale = gestureRecognizer.scale
-            columns = numberOfColumns
-        case .changed:
-            let scale = gestureRecognizer.scale
-            if scale > 1{
-                let roundedDeltaScale = Int(round(scale))
-                let temp = columns - roundedDeltaScale
-                if(temp > 1 && temp < 9 && temp != columns){
-                    numberOfColumns = temp
-                    self.collectionView?.reloadData()
-                }
-            }
-            else{
-                let roundedDeltaScale = Int(round(1/scale))
-                let temp = columns + roundedDeltaScale
-                if(temp > 1 && temp < 9 && temp != columns){
-                    numberOfColumns = temp
-                    self.collectionView?.reloadData()
-                }
-            }
-
-        default:
-            break
-        }
-    }
-    
+//    
+//    private var pinchGestureRecognizer: UIPinchGestureRecognizer!
+//    private var initialPinchScale: CGFloat = 1.0
+//    private var columns: Int = 4
+//    
+//    override func prepare() {
+//        super.prepare()
+//        setupPinchGestureRecognizer()
+//    }
+//    
+//    private func setupPinchGestureRecognizer() {
+//        pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture(_:)))
+//        pinchGestureRecognizer.delegate = self
+//        collectionView?.addGestureRecognizer(pinchGestureRecognizer)
+//    }
+//    
+//    @objc private func handlePinchGesture(_ gestureRecognizer: UIPinchGestureRecognizer) {
+//        guard collectionView != nil else { return }
+//        switch gestureRecognizer.state {
+//        case .began:
+//            initialPinchScale = gestureRecognizer.scale
+//            columns = numberOfColumns
+//        case .changed:
+//            let scale = gestureRecognizer.scale
+//            if scale > 1{
+//                let roundedDeltaScale = Int(round(scale))
+//                let temp = columns - roundedDeltaScale
+//                if(temp > 1 && temp < 9 && temp != columns){
+//                    numberOfColumns = temp
+//                    self.collectionView?.reloadData()
+//                }
+//            }
+//            else{
+//                let roundedDeltaScale = Int(round(1/scale))
+//                let temp = columns + roundedDeltaScale
+//                if(temp > 1 && temp < 9 && temp != columns){
+//                    numberOfColumns = temp
+//                    self.collectionView?.reloadData()
+//                }
+//            }
+//
+//        default:
+//            break
+//        }
+//    }
+//    
     
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         return true
@@ -77,8 +77,8 @@ class GridCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
 }
 
-extension GridCollectionViewFlowLayout: UIGestureRecognizerDelegate {
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
-}
+//extension GridCollectionViewFlowLayout: UIGestureRecognizerDelegate {
+//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return true
+//    }
+//}
