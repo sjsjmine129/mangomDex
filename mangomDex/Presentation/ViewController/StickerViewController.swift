4a7b59b9bf998df1c9c792de4f86263b89b3a2ec
sjsjmine129 <sjsjmine129@gmail.com>
2024-04-05 14:43:57 +0900

    Fix/V-VC

--- mangomDex/Presentation/ViewController/StickerViewController.swift
+++ mangomDex/Presentation/ViewController/StickerViewController.swift
@@ -39,139 +39,14 @@ class StickerViewController: UIViewController {
         return view
     }()
     
-    private lazy var lblTitle: UILabel = {
-        let title = UILabel()
-        title.translatesAutoresizingMaskIntoConstraints = false
-        title.text = "망그러진 띠부씰"
-        title.font = UIFont(name: "HUDdiu150", size: 25)
-        title.textColor = UIColor(resource: .textBlack )
-        
-        return title
-    }()
+    private lazy var vwTitle = StickerTitle()
     
-    private lazy var lblStickerNum: UILabel = {
-        var lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = UIFont(name: "HUDdiu150", size: 20)
-        lbl.textColor = UIColor(resource: .textBlack)
-        lbl.textAlignment = .center
-        
-        return lbl
-    }()
+    private lazy var btnDropdown = DropDownBtn()
     
-    private lazy var vwTitle: UIView = {
-        let vw = UIView()
-        vw.addSubview(lblTitle)
-        vw.addSubview(lblStickerNum)
-        
-        NSLayoutConstraint.activate([
-            //lblTitle
-            lblTitle.leadingAnchor.constraint(equalTo: vw.leadingAnchor),
-            lblTitle.topAnchor.constraint(equalTo: vw.topAnchor),
-            lblTitle.bottomAnchor.constraint(equalTo: vw.bottomAnchor),
-            //lblStickerNum
-            lblStickerNum.trailingAnchor.constraint(equalTo: vw.trailingAnchor),
-            lblStickerNum.bottomAnchor.constraint(equalTo: vw.bottomAnchor),
-            lblStickerNum.leadingAnchor.constraint(equalTo: lblTitle.trailingAnchor, constant: 10),
-        ])
-        
-        return vw
-    }()
+    private lazy var stNoSticker = NoStickerStack()
     
-    private let lblDropdownTitle: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "HUDdiu150", size: 15)
-        label.textColor = UIColor(resource: .textBlack)
-        return label
-    }()
     
-    private let imgVwDropDown: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    private lazy var btnDropdown: UIButton = {
-        let btn = UIButton()
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.addSubview(lblDropdownTitle)
-        btn.addSubview(imgVwDropDown)
-        
-        if let image = UIImage(systemName: "chevron.down")?.withTintColor(.textBlack, renderingMode: .alwaysOriginal) {
-            imgVwDropDown.image = image
-        }
-        
-        lblDropdownTitle.text = "전체 보기"
-        
-        btn.addTarget(self, action: #selector(showDropdown(_:)), for: .touchUpInside)
-        
-        return btn
-    }()
-    
-    private lazy var ddFilter = {
-        var dd = DropDown()
-        
-        dd.dataSource = ["전체 보기", "모은 띠부씰", "없는 띠부씰", "띠부씰 시즌1", "띠부씰 시즌2"]
-        dd.anchorView = btnDropdown
-        dd.textFont = UIFont(name: "HUDdiu150", size: 15) ?? UIFont.systemFont(ofSize: 15)
-        dd.backgroundColor = .magBody
-        dd.selectionBackgroundColor = .magMouth
-        DropDown.startListeningToKeyboard()
-        dd.cornerRadius = 5
-        dd.bottomOffset = CGPoint(x: -5, y: 45)
-        dd.selectRow(at: 0)
-        dd.cellHeight = 35
-        
-        dd.selectionAction = { [unowned self] (index: Int, item: String) in
-            lblDropdownTitle.text = item
-            
-            if let filter = StickerFilter(rawValue: item) {
-                self.stickers = stickerViewModel.filteredStickers(condition: filter)
-                lblStickerNum.text = stickerViewModel.getNumberString(stickers: self.stickers, condition: filter)
-                
-                gridFlowLayout.collectionView?.reloadData()
-                
-                self.changeImgVwDropDown(name: "chevron.down")
-            } else {}
-        }
-        
-        dd.cancelAction = { [weak self] in
-            self?.changeImgVwDropDown(name: "chevron.down")
-        }
-        
-        return dd
-    }()
-    
-    private lazy var stNoSticker: UIStackView = {
-        let stackView = UIStackView()
-        //stackView.backgroundColor = .blue
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        stackView.spacing = 10
-        
-        return stackView
-    }()
-    
-    private lazy var imgVwNoSticker: UIImageView = {
-        var imgVw = UIImageView()
-        imgVw.translatesAutoresizingMaskIntoConstraints = false
-        imgVw.contentMode = .scaleAspectFit
-        return imgVw
-    }()
-    
-    private lazy var lblNoSticker: UILabel = {
-        var lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = UIFont(name: "HUDdiu150", size: 20)
-        lbl.textColor = UIColor(resource: .textBlack)
-        lbl.textAlignment = .center
-        
-        return lbl
-    }()
-    
-    private lazy var vwBalck: UIView = {
+    private lazy var vwBlack: UIView = {
         var vw = UIView()
         vw.translatesAutoresizingMaskIntoConstraints = false
         vw.backgroundColor = .black
@@ -320,6 +195,8 @@ class StickerViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(resetStickerNumberData), name: NSNotification.Name(rawValue: "ResetStickerNumberData"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(changeCollectionNum), name: NSNotification.Name(rawValue: "ChangeCollectionNum"), object: nil)
         
+        btnDropdown.addTarget(self, action: #selector(showDropdown(_:)), for: .touchUpInside)
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         self.container = appDelegate.persistentContainer
         
@@ -367,27 +244,42 @@ class StickerViewController: UIViewController {
         self.view.addSubview(self.collectionView)
         self.view.addSubview(stNoSticker)
         
-        stNoSticker.addArrangedSubview(imgVwNoSticker)
-        stNoSticker.addArrangedSubview(lblNoSticker)
         
-        lblStickerNum.text = stickerViewModel.getNumberString(stickers: self.stickers, condition: .all)
+        vwTitle.lblStickerNum.text = stickerViewModel.getNumberString(stickers: self.stickers, condition: .all)
+        
+        btnDropdown.ddFilter.selectionAction = { [unowned self] (index: Int, item: String) in
+            btnDropdown.lblDropdownTitle.text = item
+            
+            if let filter = StickerFilter(rawValue: item) {
+                self.stickers = stickerViewModel.filteredStickers(condition: filter)
+                vwTitle.lblStickerNum.text = stickerViewModel.getNumberString(stickers: self.stickers, condition: filter)
+                
+                gridFlowLayout.collectionView?.reloadData()
+                
+                self.changeImgVwDropDown(name: "chevron.down")
+            } else {}
+        }
+        
+        btnDropdown.ddFilter.cancelAction = { [weak self] in
+            self?.changeImgVwDropDown(name: "chevron.down")
+        }
         
         // show onboarding
         if stickerViewModel.onboarding{
             stickerViewModel.onboarding = false
             
-            self.view.addSubview(vwBalck)
+            self.view.addSubview(vwBlack)
             self.view.addSubview(vwOnboarding)
             self.view.addSubview(btnClose)
             NSLayoutConstraint.activate([
-                //vwBalck
-                vwBalck.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
-                vwBalck.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
-                vwBalck.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
-                vwBalck.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+                //vwBlack
+                vwBlack.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+                vwBlack.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+                vwBlack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+                vwBlack.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
                 //vwOnboarding
-                vwOnboarding.centerXAnchor.constraint(equalTo: vwBalck.centerXAnchor),
-                vwOnboarding.centerYAnchor.constraint(equalTo: vwBalck.centerYAnchor),
+                vwOnboarding.centerXAnchor.constraint(equalTo: vwBlack.centerXAnchor),
+                vwOnboarding.centerYAnchor.constraint(equalTo: vwBlack.centerYAnchor),
                 //btnClose
                 btnClose.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
                 btnClose.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -35),
@@ -400,17 +292,6 @@ class StickerViewController: UIViewController {
             self.collectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -12),
             self.collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
             self.collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
-            //lblDropdownTitle
-            lblDropdownTitle.trailingAnchor.constraint(equalTo: btnDropdown.trailingAnchor),
-            lblDropdownTitle.bottomAnchor.constraint(equalTo: btnDropdown.bottomAnchor),
-            //imgVwDropDown
-            imgVwDropDown.trailingAnchor.constraint(equalTo: lblDropdownTitle.leadingAnchor, constant: -5),
-            imgVwDropDown.bottomAnchor.constraint(equalTo: btnDropdown.bottomAnchor),
-            imgVwDropDown.leadingAnchor.constraint(equalTo: btnDropdown.leadingAnchor, constant: 0),
-            imgVwDropDown.heightAnchor.constraint(equalToConstant: 15),
-            imgVwDropDown.widthAnchor.constraint(equalToConstant: 15),
-            //imgVwNoSticker
-            imgVwNoSticker.widthAnchor.constraint(equalToConstant: 200),
             //stNoSticker
             stNoSticker.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
             stNoSticker.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor,constant: -20),
@@ -427,20 +308,20 @@ extension StickerViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // show img
         if self.stickers.count == 0 {
-            lblNoSticker.isHidden = false
-            imgVwNoSticker.isHidden = false
+            stNoSticker.lblNoSticker.isHidden = false
+            stNoSticker.imgVwNoSticker.isHidden = false
             
-            if lblDropdownTitle.text == "모은 띠부씰"{
-                imgVwNoSticker.image = UIImage(named: "falling.png")
-                lblNoSticker.text = "아직 수집한 띠부씰이 없어요."
+            if btnDropdown.lblDropdownTitle.text == "모은 띠부씰"{
+                stNoSticker.imgVwNoSticker.image = UIImage(named: "falling.png")
+                stNoSticker.lblNoSticker.text = "아직 수집한 띠부씰이 없어요."
                 
-            }else if lblDropdownTitle.text == "없는 띠부씰"{
-                imgVwNoSticker.image = UIImage(named: "all.png")
-                lblNoSticker.text = "모든 띠부씰을 다 모았어요!"
+            }else if btnDropdown.lblDropdownTitle.text == "없는 띠부씰"{
+                stNoSticker.imgVwNoSticker.image = UIImage(named: "all.png")
+                stNoSticker.lblNoSticker.text = "모든 띠부씰을 다 모았어요!"
             }
         }else{
-            lblNoSticker.isHidden = true
-            imgVwNoSticker.isHidden = true
+            stNoSticker.lblNoSticker.isHidden = true
+            stNoSticker.imgVwNoSticker.isHidden = true
         }
         
         return self.stickers.count
@@ -494,7 +375,7 @@ private extension StickerViewController{
 extension StickerViewController{
     func changeImgVwDropDown(name: String){
         if let image = UIImage(systemName: name)?.withTintColor(.textBlack, renderingMode: .alwaysOriginal) {
-            self.imgVwDropDown.image = image
+            btnDropdown.imgVwDropDown.image = image
         }
     }
 }
@@ -504,7 +385,7 @@ extension StickerViewController{
     // show DropDown
     @objc func showDropdown(_ sender: UIButton){
         BtnAction.btnActionSize(button: sender)
-        ddFilter.show()
+        btnDropdown.ddFilter.show()
         changeImgVwDropDown(name: "chevron.up")
     }
     
@@ -528,7 +409,7 @@ extension StickerViewController{
     // close onBoarding
     @objc func closeOnBoarding(_ sender: UIButton){
         BtnAction.btnActionSize(button: sender)
-        self.vwBalck.removeFromSuperview()
+        self.vwBlack.removeFromSuperview()
         self.vwOnboarding.removeFromSuperview()
         self.btnClose.removeFromSuperview()
     }
@@ -581,9 +462,9 @@ extension StickerViewController: UIGestureRecognizerDelegate {
     }
     
     func setNumber(){
-        guard let item = lblDropdownTitle.text else{return}
+        guard let item = btnDropdown.lblDropdownTitle.text else{return}
         if let filter = StickerFilter(rawValue: item) {
-            lblStickerNum.text = stickerViewModel.getNumberString(stickers: self.stickers, condition: filter)
+            vwTitle.lblStickerNum.text = stickerViewModel.getNumberString(stickers: self.stickers, condition: filter)
         } else {}
     }
 }
